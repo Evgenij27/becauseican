a5644d74025a27ecab1bf396a28e476d43e550c2
fluffy27 <fluffy27@nxt.ru>
2018-01-18 00:31:24 +0200

    Add test for testing URL without tail

--- src/test/java/org/nashorn/server/util/response/HrefTest.java
+++ src/test/java/org/nashorn/server/util/response/HrefTest.java
@@ -4,16 +4,26 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 public class HrefTest {
 
-    private static final String URL = "http://localhost/api/test/";
+    private static final String URL_WITH_TAIL = "http://localhost/api/test/";
+    private static final String URL_WITHOUT_TAIL = "http://localhost/api/test";
 
     @Test
-    public void testHref() {
-        StringBuilder sb = new StringBuilder(URL);
+    public void testHrefWithTail() {
+        StringBuilder sb = new StringBuilder(URL_WITH_TAIL);
         Href.Builder hb = new Href.Builder(sb);
         hb.append(42L);
         Href href = hb.build();
 
-        assertEquals(URL + 42L, href.getSelf());
+        assertEquals(URL_WITH_TAIL + 42L, href.getSelf());
+    }
 
+    @Test
+    public void testHrefWithoutTail() {
+        StringBuilder sb = new StringBuilder(URL_WITHOUT_TAIL);
+        Href.Builder hb = new Href.Builder(sb);
+        hb.append(42L);
+        Href href = hb.build();
+
+        assertEquals(URL_WITHOUT_TAIL + '/' + 42L, href.getSelf());
     }
 }

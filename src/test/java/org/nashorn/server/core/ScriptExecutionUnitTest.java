56aaf9dbeab79f8079e515a145bcd4a9fdf35585
fluffy27 <fluffy27@nxt.ru>
2018-01-28 21:47:25 +0200

    Change test method for test purposes #2

--- src/test/java/org/nashorn/server/core/ScriptExecutionUnitTest.java
+++ src/test/java/org/nashorn/server/core/ScriptExecutionUnitTest.java
@@ -28,44 +28,13 @@ public class ScriptExecutionUnitTest {
         sleep();
         assertNotNull(unit.getCause());
         assertTrue(unit.finishedExceptionally());
-    }
-
-    @Test
-    public void testResultAndErrorOutput() {
-        ScriptExecutionUnit unit = mock(ScriptExecutionUnit.class);
-
-        StringWriter error = new StringWriter();
-        error.append("error");
-
-        StringWriter result = new StringWriter();
-        result.append("result");
-
-        when(unit.getErrorOutput()).thenReturn(error);
-        when(unit.getResultOutput()).thenReturn(result);
-
 
         assertNotNull(unit.getResultOutput());
-        //assertEquals("result", unit.getResultOutput().getBuffer().toString());
-
         assertNotNull(unit.getErrorOutput());
-        //assertEquals("error", unit.getErrorOutput().getBuffer().toString());
-    }
 
-    @Test
-    public void testTaskCancellation() {
-        ScriptExecutionUnit unit = mock(ScriptExecutionUnit.class);
-
-        doNothing().when(unit).cancel(true);
-        when(unit.isDone()).thenReturn(true);
-
-        unit.cancel(true);
         assertTrue(unit.isDone());
-
-        verify(unit).cancel(true);
-        verify(unit).isDone();
     }
-
-
+    
     private static void sleep() {
         try {
             Thread.sleep(1000 * 3);
